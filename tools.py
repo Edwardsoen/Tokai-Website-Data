ba4f774d5d9bfae47a5132798f29fc3aa77c2698
Edward <edwardsoen@gmail.com>
2024-06-24 22:15:50 +0700

    update data

--- tools.py
+++ tools.py
@@ -5,20 +5,20 @@ import numpy as np
 
 
 
-# path = "product-data"
-# for i in os.listdir("product-data"): 
-#     for ii in os.listdir("product-data/"+ i): 
-#         if ".jpg" in ii: 
-#             file_name = ii
-#             try: 
-#                 with Image.open(os.path.join(path, i, ii)) as img:
-#                     img.thumbnail((700, 700), Image.ANTIALIAS)
-#                     img.save(os.path.join(path, i, ii), "JPEG")
-#                     print("done :" + ii)
-#             except IOError: 
-#                 print("test")
+path = "product-data"
+for i in os.listdir("product-data"): 
+    for ii in os.listdir("product-data/"+ i): 
+        if "thumbnail.jpg" in ii: 
+            file_name = ii
+            try: 
+                with Image.open(os.path.join(path, i, ii)) as img:
+                    img.thumbnail((250, 250), Image.ANTIALIAS)
+                    img.save(os.path.join(path, i, ii), "JPEG")
+                    print("done :" + ii)
+            except IOError: 
+                print("test")
             
 
-for i in os.listdir("product-data\\Snail Horn"): 
-    img = cv2.imread(os.path.join("product-data\\Snail Horn", i))
+# for i in os.listdir("product-data\\Snail Horn"): 
+#     img = cv2.imread(os.path.join("product-data\\Snail Horn", i))
 
